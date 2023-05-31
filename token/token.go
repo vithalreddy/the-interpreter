87fa68354dafcdd2706b4232bbc09e12cd725c1d
Vithal Reddy <vmreddyvmb@gmail.com>
2023-05-31 15:50:00 +0530

    feat: fmt

--- token/token.go
+++ token/token.go
@@ -1,47 +1,46 @@
 package token
 
 const (
-  ILLEGAL = "ILLEGAL"
-  EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  // Identifiers + literals
-  IDENT = "IDENT" // add, foobar, x, y, ...
-  INT = "INT" // 1343456
-  
-  // Operators
-  ASSIGN = "="
-  PLUS = "+"
-  
-  // Delimiters
-  COMMA = ","
-  SEMICOLON = ";"
-  LPAREN = "("
-  RPAREN = ")"
-  LBRACE = "{"
-  RBRACE = "}"
-  
-  // Keywords
-  FUNCTION = "FUNCTION"
-  LET = "LET"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
 
 type TokenType string
 
-
 type Token struct {
-  Type TokenType
-  Literal string
+	Type    TokenType
+	Literal string
 }
 
 var Keywords = map[string]TokenType{
-  "fn": FUNCTION,
-  "let": LET,
+	"fn":  FUNCTION,
+	"let": LET,
 }
 
 func LookupIdent(ident string) TokenType {
-  if tok, ok := Keywords[ident]; ok {
-    return tok
-  }
+	if tok, ok := Keywords[ident]; ok {
+		return tok
+	}
 
-  return IDENT
+	return IDENT
 }
