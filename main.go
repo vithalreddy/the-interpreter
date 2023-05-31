63d87034c75377c0c460f09b995068bc003e3d9a
Vithal Reddy <vmreddyvmb@gmail.com>
2023-05-31 17:44:52 +0530

    feat: repl basic io added :zap:

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"luza/repl"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!\n", user.Username)
+	fmt.Printf("This is the Luza programming language!\n")
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
